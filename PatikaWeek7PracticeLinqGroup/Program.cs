7f4c2d12fdd86a6635b15ebde521cf2cd4b55843
Bilal <bilalbilin12@gmail.com>
2024-08-29 16:07:20 +0300

    Add project files.

---
+++ PatikaWeek7PracticeLinqGroup/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using PatikaWeek7PracticeLinqGroup;
+
+List<Student> students = new List<Student>(); // Öğrenci listesi
+
+students.Add(new Student { StudentId = 1, StudentName = "Ali", ClassId = 1 });
+students.Add(new Student { StudentId = 2, StudentName = "Ayşe", ClassId = 2 });
+students.Add(new Student { StudentId = 3, StudentName = "Mehmet", ClassId = 1 });
+students.Add(new Student { StudentId = 4, StudentName = "Fatma", ClassId = 3 });
+students.Add(new Student { StudentId = 5, StudentName = "Ahmet", ClassId = 2 });
+
+ 
+List<Class> classes = new List<Class>(); // Sınıf Listesi
+
+classes.Add(new Class { ClassId = 1, ClassName = "Matematik" });
+classes.Add(new Class { ClassId = 2, ClassName = "Türkçe" });
+classes.Add(new Class { ClassId = 3, ClassName = "Kimya" });
+
+
+
+// GroupJoin metoduyla verili sınıflara kayıtlı öğrencilerin sorgusu yapıldı
+
+var studentsWithClasses = classes.GroupJoin(students,
+                                            @class => @class.ClassId,
+                                            student => student.ClassId,
+                                            (@class, studentGroup) => new
+                                            {
+                                                Class = @class,
+                                                Students = studentGroup.ToList()
+                                            });
+
+foreach (var item in studentsWithClasses)
+{
+    Console.WriteLine($"-- Sınıf: {item.Class.ClassName}");
+    foreach (var student in item.Students)
+    {
+        Console.WriteLine($"Öğrenci: {student.StudentName}");
+    }
+
+}
+                                                                          
---
+++ PatikaWeek7PracticeLinqGroup/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaWeek7PracticeLinqGroup
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public int ClassId { get; set; }
+
+    }
+
+    public class Class
+    {
+        public int ClassId { get; set; }
+        public string ClassName { get; set; }
+    }
+}
